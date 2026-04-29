d9e5c433942c74f706946f9a0fef59932f98dc1d
Stefan-Orn <stebbio3@gmail.com>
2026-04-29 11:37:30 +0000

    Kóði til að test'a hvort servo'ar virki

--- servo.py
+++ servo.py
@@ -1,5 +1,11 @@
 import time
 from adafruit_servokit import ServoKit
 
+# Initialize the PCA9685 for 16 channels
+kit = ServoKit(channels=16)
 
+# Set the servo on channel 0 to 0 degrees
+print("Moving to 0 degrees")
+kit.servo[0].angle = 0
+time.sleep(1)
 
