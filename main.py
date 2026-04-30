e3c0bcf77d4231c71ad164ca25660b99fdb53647
Dagur <dagur25@ru.is>
2026-04-30 13:19:07 +0000

    laga main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import SRF02 as distanceSensors
-import servo as servoMotors
+#import SRF02 as distanceSensors
+#import servo as servoMotors
 from motor import send_motors, forward, backwards, right, left, stop
 import time
 
