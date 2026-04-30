991b8a4f9ec038e549343eb70c15b5a564a78b5c
Stefan-Orn <stebbio3@gmail.com>
2026-04-30 09:09:56 +0000

    Lítil breyting í distance sensor

--- SRF02.py
+++ SRF02.py
@@ -6,14 +6,14 @@ def distance_scan():
     # (GPIO 2 og 3)
     i2c_addresses = [0x70, 0x71] # Listi yfir addressur á sensorum
     while True:
-        bus.write_byte_data(i2c_addresses[0], 0, 81) # Kveikji á vinstri skynjara
-        bus.write_byte_data(i2c_addresses[1], 0, 81) # Kveikji á hægri skynjara
+        bus.write_byte_data(i2c_addresses[0], 0, 0x51) # Kveikji á vinstri skynjara
+        bus.write_byte_data(i2c_addresses[1], 0, 0x51) # Kveikji á hægri skynjara
         high_v = bus.read_byte_data(i2c_addresses[0],2)
         high_h = bus.read_byte_data(i2c_addresses[1],2)
 
         low_v = bus.read_byte_data(i2c_addresses[0],3)
         low_h = bus.read_byte_data(i2c_addresses[1],3)
-        print("Vinstri:",high_v * 256 + low_v, 3," Hægri:", high_h * 256 + low_h)
+        print("Vinstri:",high_v * 256 + low_v," Hægri:", high_h * 256 + low_h)
         time.sleep(0.1)
 
 distance_scan()
