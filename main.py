b5b547267e5831498bb9975d4c477f4b381d9615
Dagur <dagur25@ru.is>
2026-04-30 13:16:31 +0000

    prufa main

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import SRF02 as distanceSensors
 import servo as servoMotors
 from motor import send_motors, forward, backwards, right, left, stop
+import time
 
+forward()
+time.sleep(5)
+right()
+time.sleep(2)
+backwards()
+time.sleep(3)
+stop()
