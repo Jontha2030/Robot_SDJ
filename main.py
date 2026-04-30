27b6e5583dd5687a1968429e987b8b5df12904fc
Dagur <dagur25@ru.is>
2026-04-30 14:04:56 +0000

    prufa keys í main

--- main.py
+++ main.py
@@ -2,11 +2,37 @@
 #import servo as servoMotors
 from motor import send_motors, forward, backwards, right, left, stop
 import time
+import pygame
 
-forward()
-time.sleep(5)
-right()
-time.sleep(2)
-backwards()
-time.sleep(3)
-stop()
+running = True
+while running:
+    # Handle events (VERY important)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            stop()
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                stop()
+                running = False
+
+    # Check held keys
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        forward()
+
+    elif keys[pygame.K_DOWN]:
+        backwards()
+
+    elif keys[pygame.K_RIGHT]:
+        right()
+
+    elif keys[pygame.K_LEFT]:
+        left()
+
+    else:
+        stop()
+
+pygame.quit()
