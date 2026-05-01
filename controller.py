f0e6d1e19161b6c6553abbb0a5e5d736e955f74d
Dagur <dagur25@ru.is>
2026-05-01 10:40:12 +0000

    controller update 3

--- controller.py
+++ controller.py
@@ -1,21 +1,21 @@
-from pyPS4Controller.controller import Controller
+from evdev import InputDevice, ecodes
 
-print("Starting controller...")
+dev = InputDevice("/dev/input/event4")
 
-class MyController(Controller):
-    def on_x_press(self):
-        print("X pressed")
+BTN_X = 304
+BTN_CIRCLE = 305
+BTN_TRIANGLE = 307
+BTN_SQUARE = 308
 
-    def on_circle_press(self):
-        print("Circle pressed")
+print("Controller ready")
 
-    def on_triangle_press(self):
-        print("Triangle pressed")
-
-    def on_square_press(self):
-        print("Square pressed")
-
-controller = MyController(interface="/dev/input/event4", connecting_using_ds4drv=False)
-print("Listening...")
-controller.listen(timeout=60)
-print("Stopped")
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY and event.value == 1:
+        if event.code == BTN_X:
+            print("X pressed")
+        elif event.code == BTN_CIRCLE:
+            print("Circle pressed")
+        elif event.code == BTN_TRIANGLE:
+            print("Triangle pressed")
+        elif event.code == BTN_SQUARE:
+            print("Square pressed")
