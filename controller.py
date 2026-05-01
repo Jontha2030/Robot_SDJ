323091feb9e22d46dbdf99fa99a17f258f8ba975
Dagur <dagur25@ru.is>
2026-05-01 12:38:42 +0000

    controller update 9

--- controller.py
+++ controller.py
@@ -1,8 +1,11 @@
 from evdev import InputDevice, ecodes
 from motor import send_motors, forward, backwards, right, left, stop
+
+#Fall fyrir controller
 def controller_sturcture():
     dev = InputDevice("/dev/input/event4")
 
+    #Skillgreini takka
     BTN_X = 304
     BTN_CIRCLE = 305
     BTN_TRIANGLE = 307
@@ -16,22 +19,19 @@ def controller_sturcture():
 
     print("Controller ready")
 
+
+    #Ef ýtt er á taka þá gerist eitthvað
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY and event.value == 1:
             if event.code == BTN_X:
-                print("X pressed")
                 backwards()
             elif event.code == BTN_CIRCLE:
-                print("Circle pressed")
                 right()
             elif event.code == BTN_TRIANGLE:
-                print("Triangle pressed")
                 forward()
             elif event.code == BTN_SQUARE:
-                print("Square pressed")
                 left()
             elif event.code == BTN_R1:
-                print("R1 pressed")
                 stop()
             elif event.code == BTN_R2:
                 print("R2 pressed")
@@ -42,6 +42,8 @@ def controller_sturcture():
             elif event.code == BTN_R3:
                 print("R3 pressed")
 
+
+        #Þetta er fyrir D-pad
         elif event.type == ecodes.EV_ABS:
             if event.code == ecodes.ABS_HAT0Y:
                 if event.value == -1:
