a6bb24bebc83db97c1cefd6b2a075514c6f6048c
Dagur <dagur25@ru.is>
2026-05-01 10:47:50 +0000

    controller update 4

--- controller.py
+++ controller.py
@@ -7,6 +7,12 @@ BTN_CIRCLE = 305
 BTN_TRIANGLE = 307
 BTN_SQUARE = 308
 
+BTN_L1 = 310
+BTN_R1 = 311
+BTN_L2 = 312
+BTN_R2 = 313
+BTN_R3 = 318
+
 print("Controller ready")
 
 for event in dev.read_loop():
@@ -19,3 +25,13 @@ for event in dev.read_loop():
             print("Triangle pressed")
         elif event.code == BTN_SQUARE:
             print("Square pressed")
+        elif event.code == BTN_R1:
+            print("R1 pressed")
+        elif event.code == BTN_R2:
+            print("R2 pressed") 
+        elif event.code == BTN_L1:
+            print("L1 pressed")
+        elif event.code == BTN_L2:
+            print("L2 pressed")
+        elif event.code == BTN_R3:
+            print("R3 pressed")
