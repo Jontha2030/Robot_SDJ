4399b5ffe19ecdb60f9f5b7c95b051188ae23afb
Dagur <dagur25@ru.is>
2026-05-01 13:53:41 +0000

    prufa servo 7

--- servo.py
+++ servo.py
@@ -56,13 +56,13 @@ def selfturning_servos():
 
 
     while True:
-        for angle in range(90, 0, 10):
+        for angle in range(90, -10, 10):
             kit.servo[0].angle = angle
             kit.servo[1].angle = 90 + (90-angle)
             time.sleep(0.05)
 
         time.sleep(0.2)   
-        for angle in range(0, 90, 10):
+        for angle in range(0, 100, 10):
             kit.servo[0].angle = angle
             kit.servo[1].angle = 90 + (90-angle)
             time.sleep(0.05)
