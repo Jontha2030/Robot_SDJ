aa9c6074bc4035b31ea9fd16a5f8ba6ddc4aee90
Stefan-Orn <stebbio3@gmail.com>
2026-05-04 13:04:03 +0000

    aðlögun

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
 from evdev import InputDevice, ecodes
-from motor import forward, backwards, right, left, stop
-from __init__ import Button_Press
-from avoid_obstacles import avoid_obstacles
+from motor import send_motors, forward, backwards, right, left, stop
+from play import play_random, get_songs, start_playing
 
+speed = 200
 #Fall fyrir controller
 def controller_sturcture():
     dev = InputDevice("/dev/input/event4")
@@ -26,24 +26,21 @@ def controller_sturcture():
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY and event.value == 1:
             if event.code == BTN_X:
-                backwards()
+                backwards(speed)
             elif event.code == BTN_CIRCLE:
-                right()
+                right(speed)
             elif event.code == BTN_TRIANGLE:
-                forward()
+                forward(speed)
             elif event.code == BTN_SQUARE:
-                left()
+                left(speed)
             elif event.code == BTN_R1:
                 stop()
             elif event.code == BTN_R2:
                 print("R2 pressed")
             elif event.code == BTN_L1:
                 print("L1 pressed")
-                print("Entering AUTO-MODE")
-                avoid_obstacles()
             elif event.code == BTN_L2:
                 print("L2 pressed")
-                Button_Press["state"] = True
             elif event.code == BTN_R3:
                 print("R3 pressed")
 
@@ -60,23 +57,10 @@ def controller_sturcture():
                 if event.value == -1:
                     print("D-pad left")
                 elif event.value == 1:
+                    play_random()
                     print("D-pad right")
-                    
-                    
-<<<<<<< HEAD
-if __name__ == "__main__":
-    controller_sturcture()
-        
-=======
-    # Hér er gripið errora og þegar notandi slekkur á forritinu og séð til þess að slökkt er á mótórum
-    except Exception as keyrsluError:
-        print("Ehv. for úrskeiðis", keyrsluError)
-        stop()
-        
-    except KeyboardInterrupt:
-        print("Notandi slökkti á forriti")
-        stop()
 
 
-avoid_obstacles()
->>>>>>> 3302831 (aðlögun)
+#controller_sturcture()
+
+
