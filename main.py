c9888960339c96bd22fe2622ed8bd153114345c2
Stefan-Orn <stebbio3@gmail.com>
2026-05-04 09:03:23 +0000

    Commentað'i main kóðann og bætti við smá villu vörðum

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import threading
 from servo import selfturning_servos, servo_init
 from __init__ import SRF02_data, lock
     
+    
+# ---------Global breytur------------
+EFRIMÖRK = 40 # cm, Fjarlægð sem róbót byrjar að beygja við
 
 def keyra_bil():
   while True:
@@ -42,54 +45,63 @@ def speakers():
     speaker.play()
 
 def avoid_obstacles():
-    SRF02thread = threading.Thread(target=distance_scan, daemon=True)
-    servothread = threading.Thread(target=selfturning_servos, daemon=True)
-    SRF02thread.start()
-    servothread.start()
-    time.sleep(0.8)
+    # Bý til tvo threads þar sem að eftirfarandi tveir hlutir keyra  með while loopum
+    try:
+        SRF02thread = threading.Thread(target=distance_scan, daemon=True) # Einn fyrir SRF02 fjarlægðarskynjarann
+        servothread = threading.Thread(target=selfturning_servos, daemon=True) # Einna fyrir servoana
+        SRF02thread.start()
+        servothread.start()
+        time.sleep(0.8)
     
-    servo_init([0,1])
-
-    current_state = None
+        servo_init([0,1]) # Þetta virkjar servo'a og gefur þeim upphafsstöðuna 90°, sem er miðjan á bili þeirra (0-180°)
+    except Exception as InitError:
+        print("Einhvað fór úrsskeiðis við virkjun: ", InitError)
+    
+    current_state = None # Nota þessa breytu til þess að þurfa ekki að senda boð á Motorcontroller'a aftur og aftur
     try:
         while True:
-            with lock:
-                distance_v = SRF02_data["left"]
+            with lock: # Lock er tengt threads
+                # Hér eru thread breyturnar tengdar fjarlægðarskynjaranum en þær geyma mælda fjarlægð
+                distance_v = SRF02_data["left"] 
                 distance_h = SRF02_data["right"]
                 
-            if distance_v is None or distance_h is None:
+            if distance_v is None or distance_h is None: # Þetta er til þess að forrit chrash'ar ekki í fyrstu umferð, en þá skilar SFR02 forritið studnum None
                 time.sleep(0.1)
                 continue
             
-            print("Vinstri:",distance_v," Hægri:",distance_h) #----Debug
-            if 1 < distance_v < 30:
+            #print("Vinstri:",distance_v," Hægri:",distance_h) #----Debug
+            # Hér kemur logic'ið til þess að forðast hluti (valdar fjarlægðir fundust með að prufa)
+            if 1 < distance_v < EFRIMÖRK: # Athugar hvort vinstri skynjari sé innan marka 
                 if current_state != "beygja":
-                    print("STOP! Beygji til vinstri") #----Debug
+                    #print("STOP! Beygji til vinstri") #----Debug
+                    # Kalla á föllin sem keyra mótórana með Motor controllernum
                     stop()
                     time.sleep(0.01)
                     right()
                     current_state = "beygja"
                     
-            elif 1 < distance_h < 30:
+            elif 1 < distance_h < EFRIMÖRK: # Athugar hvort hægri skynjari sé innan marka
                 if current_state != "beygja":
-                    print("STOP! Beygji til hægri") #----Debug
+                    #print("STOP! Beygji til hægri") #----Debug
+                    # Kalla á föllin sem keyra mótórana með Motor controllernum
                     stop()
                     time.sleep(0.01)
                     left()
                     current_state = "beygja"
                 
-            else:
+            else: # Ef engin hætta er skynjuð, keyrir bíllinn bara áfram
                 if current_state != "afram":
-                    print("You good, áfram!")
+                    #print("You good, áfram!") #----Debug
                     forward()
                     current_state = "afram"
                     
-    except Exception as e:
-        print("Ehv. for úrskeiðis", e)
+    # Hér er gripið errora og þegar notandi slekkur á forritinu og séð til þess að slökkt er á mótórum
+    except Exception as keyrsluError:
+        print("Ehv. for úrskeiðis", keyrsluError)
         stop()
         
     except KeyboardInterrupt:
-        print("Forrit er hætt")
+        print("Notandi slökkti á forriti")
         stop()
 
 
