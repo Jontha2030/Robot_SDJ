a96021713388cd051f05bebe4b32713b5aa6be79
Stefan-Orn <stebbio3@gmail.com>
2026-05-04 13:53:44 +0000

    Ehv. að distance sensers

--- main.py
+++ main.py
@@ -7,66 +7,76 @@ import avoid_obstacles
 speed = 200
 #Fall fyrir controller
 def controller_sturcture():
-    dev = InputDevice("/dev/input/event4")
+    try:
+        dev = InputDevice("/dev/input/event4")
 
-    #Skillgreini takka
-    BTN_X = 304
-    BTN_CIRCLE = 305
-    BTN_TRIANGLE = 307
-    BTN_SQUARE = 308
+        #Skillgreini takka
+        BTN_X = 304
+        BTN_CIRCLE = 305
+        BTN_TRIANGLE = 307
+        BTN_SQUARE = 308
 
-    BTN_L1 = 310
-    BTN_R1 = 311
-    BTN_L2 = 312
-    BTN_R2 = 313
-    BTN_R3 = 318
+        BTN_L1 = 310
+        BTN_R1 = 311
+        BTN_L2 = 312
+        BTN_R2 = 313
+        BTN_R3 = 318
 
-    print("Controller ready")
-
-
-    #Ef ýtt er á taka þá gerist eitthvað
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY and event.value == 1:
-            if event.code == BTN_X:
-                backwards(speed)
-            elif event.code == BTN_CIRCLE:
-                right(speed)
-            elif event.code == BTN_TRIANGLE:
-                forward(speed)
-            elif event.code == BTN_SQUARE:
-                left(speed)
-            elif event.code == BTN_R1:
-                stop()
-            elif event.code == BTN_R2:
-                print("R2 pressed")
-            elif event.code == BTN_L1:
-                Button_Press["state"] = False
-                print("L1 pressed")
-                print("Entering AUTO-MODE")
-                avoid_obstacles.avoid_obstacles()
-            elif event.code == BTN_L2:
-                print("L2 pressed")
-                Button_Press["state"] = True
-            elif event.code == BTN_R3:
-                print("R3 pressed")
+        print("Controller ready")
 
 
-        #Þetta er fyrir D-pad
-        elif event.type == ecodes.EV_ABS:
-            if event.code == ecodes.ABS_HAT0Y:
-                if event.value == -1:
-                    print("D-pad up")
-                elif event.value == 1:
-                    print("D-pad down")
+        #Ef ýtt er á taka þá gerist eitthvað
+        for event in dev.read_loop():
+            if event.type == ecodes.EV_KEY and event.value == 1:
+                if event.code == BTN_X:
+                    backwards(speed)
+                elif event.code == BTN_CIRCLE:
+                    right(speed)
+                elif event.code == BTN_TRIANGLE:
+                    forward(speed)
+                elif event.code == BTN_SQUARE:
+                    left(speed)
+                elif event.code == BTN_R1:
+                    stop()
+                elif event.code == BTN_R2:
+                    print("R2 pressed")
+                elif event.code == BTN_L1:
+                    Button_Press["state"] = False
+                    print("L1 pressed")
+                    print("Entering AUTO-MODE")
+                    avoid_obstacles.avoid_obstacles()
+                elif event.code == BTN_L2:
+                    print("L2 pressed")
+                    Button_Press["state"] = True
+                    SRF02_data["left"] = None
+                    SRF02_data["right"] = None
+                    stop()
+                elif event.code == BTN_R3:
+                    print("R3 pressed")
 
-            elif event.code == ecodes.ABS_HAT0X:
-                if event.value == -1:
-                    stop_playing()
-                    print("D-pad left")
-                elif event.value == 1:
-                    play_random()
-                    print("D-pad right")
-                    
+
+            #Þetta er fyrir D-pad
+            elif event.type == ecodes.EV_ABS:
+                if event.code == ecodes.ABS_HAT0Y:
+                    if event.value == -1:
+                        print("D-pad up")
+                    elif event.value == 1:
+                        print("D-pad down")
+
+                elif event.code == ecodes.ABS_HAT0X:
+                    if event.value == -1:
+                        stop_playing()
+                        print("D-pad left")
+                    elif event.value == 1:
+                        play_random()
+                        print("D-pad right")
+                        
+    except KeyboardInterrupt:
+        print("Notandi hætti forriti")
+        stop()
+        Button_Press["state"] = False
+
+                        
 
 if __name__ == "__main__":
     controller_sturcture()
