8158a63dffbdeb41a03ae0e4b7331859f1541926
Stefan-Orn <stebbio3@gmail.com>
2026-05-11 12:29:22 +0000

    Ennþ´að breyta hraað

--- motor.py
+++ motor.py
@@ -28,13 +28,13 @@ def check_speed(speed):
 #Seigir mótor að fara áfram
 def forward(speed):
     speed = check_speed(speed)
-    send_motors(speed + 15,-speed-10)
+    send_motors(speed + 8,-speed-14)
 
 
 #Seigir mótor að fara afturábaka
 def backwards(speed):
     speed = check_speed(speed)
-    send_motors(-(speed + 15), speed-7)
+    send_motors(-(speed + 14), speed-7)
 
 
 #Seigir mótor að fara til hægri
